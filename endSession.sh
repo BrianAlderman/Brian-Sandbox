8c0e0b79079c00f0b61f3afdb4cdd7e50f800d47
balderman90 <balderman90@gmail.com>
2016-01-05 18:12:06 -0500

    $message

--- endSession.sh
+++ endSession.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+git commit --dry-run 
+echo -n "List of files correct? [Y/N]"
+read fileCorrect
+if [$fileCorrect != Y]
+  then 
+    echo "Commit process exited"
+    exit
+fi
+
 echo -n "Enter your commit message (no quotes) and press [ENTER]: "
 read message
 echo "$message"
