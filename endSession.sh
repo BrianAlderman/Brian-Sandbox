797d18ef1154bcdab1ef838a59d02cd84bad8b82
balderman90 <balderman90@gmail.com>
2016-01-05 18:20:26 -0500

    But doing it properly this time

--- endSession.sh
+++ endSession.sh
@@ -12,5 +12,5 @@ echo -n "Enter your commit message (no quotes) and press [ENTER]: "
 read message
 echo "Creating commit with note $message"
 
-git commit -m '$message' -a #creates the commit
+git commit -m "$message" -a #creates the commit
 git push #pushes to github
