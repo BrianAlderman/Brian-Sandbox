a08d23e1704dcfd5616ee13c0ff24937ed659195
balderman90 <balderman90@gmail.com>
2016-01-05 18:19:34 -0500

    $message

--- endSession.sh
+++ endSession.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git commit --dry-run -a 
-echo -n "List of files correct? [Y/N]"
+echo -n "List of files correct? [Y/N]: "
 read fileCorrect
-if [$fileCorrect != "Y"]
+if [ "$fileCorrect" != "Y" ];
   then 
     echo "Commit process exited"
     exit 1
@@ -10,7 +10,7 @@ fi
 
 echo -n "Enter your commit message (no quotes) and press [ENTER]: "
 read message
-echo "$message"
+echo "Creating commit with note $message"
 
 git commit -m '$message' -a #creates the commit
 git push #pushes to github
