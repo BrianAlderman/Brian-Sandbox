f8247cbd00eef8156ef89d9efb5a639f673d17ee
balderman90 <balderman90@gmail.com>
2016-01-05 18:03:53 -0500

    $message

--- endSession.sh
+++ endSession.sh
@@ -3,5 +3,5 @@ echo -n "Enter your commit message (no quotes) and press [ENTER]: "
 read message
 echo "$message"
 
-git commit -m '$message' #creates the commit
+git commit -m '$message' -a #creates the commit
 git push #pushes to github
